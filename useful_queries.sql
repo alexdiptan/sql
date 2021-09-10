3c58872dccd0fe072ee205461e5c39802b5136de
alexdiptan <85482697+alexdiptan@users.noreply.github.com>
2021-09-10 09:37:49 +0300

    Update useful_queries.sql

--- useful_queries.sql
+++ useful_queries.sql
@@ -1,2 +1,3 @@
 -- используем функцию TIMESTAMPDIFF для вычисления возраста из дня рожения.
 SELECT TIMESTAMPDIFF(YEAR, pf.birthday, NOW()) FROM profiles pf
+-- раскрыть группу можно при помощи функции GROUP_CONCAT или ARRAY_CONCAT
